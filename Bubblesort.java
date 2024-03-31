f8c93784dcc6f7130946d88c1e93f1801fc11058
Jatin Gupta <140484456+Jatin2753@users.noreply.github.com>
2024-03-31 21:50:48 +0530

    java practice
    java practice

---
+++ Bubblesort.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Bubblesort {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n, temp;
+        System.out.println("enter number of elements in array");
+        n = s.nextInt();
+        int ar[] = new int[n];
+        System.out.println("enter elements");
+        for (int i = 0; i < n; i++) {
+            ar[i] = s.nextInt();
+        }
+        for (int i = 0; i < ar.length - 1; i++) {
+            for (int j = 0; j < ar.length - i - 1; j++) {
+                System.out.print(ar[i]);
+                System.out.println(ar[i + 1]);
+                if (ar[j] > ar[j + 1]) {
+                    temp = ar[j];
+                    ar[j] = ar[j + 1];
+                    ar[j + 1] = temp;
+                }
+            }
+
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(ar[i]);
+        }
+    }
+}
