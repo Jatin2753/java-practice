9bdbdd1972748719572bb82d87f9294c9e7f9ad1
Jatin Gupta <140484456+Jatin2753@users.noreply.github.com>
2024-03-31 21:53:34 +0530

    Update Bubblesort.java
    java practice

--- Bubblesort.java
+++ Bubblesort.java
@@ -15,8 +15,6 @@ public class Bubblesort {
         }
         for (int i = 0; i < ar.length - 1; i++) {
             for (int j = 0; j < ar.length - i - 1; j++) {
-                System.out.print(ar[i]);
-                System.out.println(ar[i + 1]);
                 if (ar[j] > ar[j + 1]) {
                     temp = ar[j];
                     ar[j] = ar[j + 1];
